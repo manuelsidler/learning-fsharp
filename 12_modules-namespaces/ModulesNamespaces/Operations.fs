a851cc8f9b57cda0c41f452ea8c71a719fab2caf
Manuel Sidler <sidler.manuel@gmail.com>
2018-05-31 22:38:49 +0200

    add sample for modules and namespaces

---
+++ 12_modules-namespaces/ModulesNamespaces/Operations.fs
@@ -0,0 +1,8 @@
+﻿module Operations
+open Domain
+
+let getInitials customer =
+    customer.FirstName.[0], customer.LastName.[0]
+
+let isOlderThan age customer =
+    customer.Age > age
---
+++ 12_modules-namespaces/ModulesNamespaces/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Domain
+open Operations
+
+[<EntryPoint>]
+let main argv =
+    let customer = { FirstName = "Chuck"; LastName = "Norris"; Age = 50 }
+    if customer |> isOlderThan 18 then printfn "%s is an adult" customer.FirstName
+    else printf "%s is a child." customer.FirstName
+    0 // return an integer exit code
+    
