1760101310b86a061f38b4f823bdc14b5fd4e508
Manuel Sidler <sidler.manuel@gmail.com>
2018-07-03 21:21:08 +0200

    add exercise for mapping and sorting

--- 16_exercise.fsx
+++ 16_exercise.fsx
@@ -1,26 +1,17 @@
 open System.IO
-open System
-
-// type FolderStatistic = { Name : string 
-//                          Size : int
-//                          NumberOfFiles : int 
-//                          AverageFileSize : int
-//                          FileExtensions : string list }
 
 type FolderStatistic = { Name : string 
                          Size : int64 }
 
 let calculateFolderSize (folder : string) =
-    let files = Directory.GetFiles folder |> List.ofSeq
-    files |> List.sumBy (fun f -> FileInfo(f).Length)
+    folder
+    |> Directory.GetFiles
+    |> Array.sumBy (fun f -> FileInfo(f).Length)
    
+let getSubfolders path =
+    let folder = DirectoryInfo path
+    folder.GetDirectories("*", SearchOption.AllDirectories)
 
-let subfolders : DirectoryInfo array =
-    let folder = new DirectoryInfo(@"C:\\tmp")
-    folder.GetDirectories()
-
-subfolders
-|> List.ofArray
-// group by?
-|> List.map (fun x -> { Name = x.Name; Size = calculateFolderSize x.FullName })
-|> List.sortByDescending(fun x -> x.Size)
+getSubfolders @"C:\tmp"
+|> Array.map (fun x -> { Name = x.Name; Size = calculateFolderSize x.FullName })
+|> Array.sortByDescending(fun x -> x.Size)
