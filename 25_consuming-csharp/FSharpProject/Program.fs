2a57cb3ab5f5efdaaafe050f3c36ba7d4b91d8b9
Manuel Sidler <sidler.manuel@gmail.com>
2019-07-26 14:02:00 +0200

    add samples for C# consumption

---
+++ 25_consuming-csharp/FSharpProject/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open CSharpProject
+
+[<EntryPoint>]
+let main argv =
+    let tony = Person "Tony"
+    tony.PrintName();
+
+    0 // return an integer exit code
---
+++ 25_consuming-csharp/Scratchpad.fsx
@@ -0,0 +1,40 @@
+﻿#r @"CSharpProject/bin/Debug/netstandard2.0/CSharpProject.dll"
+
+open CSharpProject
+open System
+open System.Collections.Generic
+
+// consuming C# objects and methods
+let manuel = Person "Manuel"
+manuel.PrintName()
+
+// constructors as functions
+let people =
+    [ "Manuel"; "Christine"; "Sarah" ]
+    |> List.map Person
+
+people |> List.iter (fun p -> p.PrintName())
+
+// interfaces
+type PersonComparer() =
+    interface IComparer<Person> with
+        member this.Compare(x, y) = x.Name.CompareTo(y.Name)
+
+let personComparer = PersonComparer() :> IComparer<Person>
+personComparer.Compare(manuel, Person "Michael")
+personComparer.Compare(manuel, Person "Manuel")
+
+// object expressions
+let pComparer = 
+    { new IComparer<Person> with
+        member this.Compare(x, y) = x.Name.CompareTo(y.Name) }
+
+// Nulls, nullables and options
+let blank:string = null
+let name = "Mona"
+let number = Nullable 10
+
+let blankAsOption = blank |> Option.ofObj // Null maps to None
+let nameAsOption = name |> Option.ofObj // Non-null maps to Some
+let numberAsOption = number |> Option.ofNullable
+let unsafeName = Some "Fred" |> Option.toObj // Map options back to classes or nullable types
