17383634d6ddfa65033930063be4b596f64a727e
Manuel Sidler <sidler.manuel@gmail.com>
2019-07-27 15:06:04 +0200

    add samples for c# interop

---
+++ 27_expose-to-csharp/CSharpApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FSharpCode;
+
+namespace CSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car(10, "Audi", Tuple.Create(2.5, 3.7)); // Go to definition will show implemented interfaces like IEquatable
+
+            var motorbike = Vehicle.NewMotorbike("Harley", 4.0); // returns Vehicle, not Motorbike
+            var isMotorbike = motorbike.IsMotorbike;
+
+            // C# pattern matching
+            switch (motorbike)
+            {
+                case Vehicle.Motorbike mb : break;
+                case Vehicle.Motorcar mc: break;
+                default:
+                    break;
+            }
+
+            // Call F# functions
+            // F# modules are renderes as static classes
+            var car2 = Functions.CreateCar(2, "BMW", 4.0, 6.5);
+            var car3 = Functions.CreateFourWheeledCar // curried function needs to be invoked for every parameter
+                .Invoke("Mercedes")
+                .Invoke(1.5)
+                .Invoke(2.5);
+        }
+    }
+}
---
+++ 27_expose-to-csharp/FSharpCode/Library.fs
@@ -0,0 +1,23 @@
+﻿namespace FSharpCode
+
+type Car =
+    { 
+        /// Number of wheels on the car. 
+        Wheels : int
+
+        /// Brand of the car
+        Brand : string
+
+        /// The x/y of the car in meters
+        Dimensions: float * float
+    }
+
+type Vehicle =
+    | Motorcar of Car
+    | Motorbike of Name:string * EngineSize:float
+
+module Functions =
+    let CreateCar wheels brand x y =
+        { Wheels = wheels; Brand = brand; Dimensions = x, y }
+
+    let CreateFourWheeledCar = CreateCar 4
